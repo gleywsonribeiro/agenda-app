5f7b89d48523f86de140ade56636fedec090e8c8
Gleywson Ribeiro <148725221+gleywson-softsy@users.noreply.github.com>
2025-02-16 00:50:40 -0300

    Criada a tela de registro

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth.service';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [
+    FormsModule
+  ]
 })
 export class LoginComponent {
   email = '';
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  nome = '';
+  email = '';
+  senha = '';
+  erro = '';
 
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  register() {
+    this.authService.register(this.nome, this.email, this.senha).subscribe({
+      next: () => this.router.navigate(['/auth/login']),
+      error: () => this.erro = 'Falha no registro. Tente novamente.'
+    });
+  }
 }
