240bf7c39e7e48c9d563f3afe79a4a8717f41041
Gleywson Ribeiro <148725221+gleywson-softsy@users.noreply.github.com>
2025-02-16 00:33:35 -0300

    Configuração inicial do projeto agenda-app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ListaComponent } from './compromissos/lista/lista.component';
+import { DetalhesComponent } from './compromissos/detalhes/detalhes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'compromissos', component: ListaComponent },
+  { path: 'compromissos/:id', component: DetalhesComponent },
+  { path: '**', redirectTo: 'auth/login' } // Redireciona para login se a rota não existir
+];
